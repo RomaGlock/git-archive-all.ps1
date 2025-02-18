6884676fdc48bd67596bd194e6d2a166e1435706
Roman Makarov <roman.makarov@phystech.edu>
2025-02-18 20:17:31 +0300

    Many changes! - work with any submodules nesting levels (actually i checked 2 levels :) ) - check if repo clean (don't archive dirty repos) - create folder for temporary archives (hate garbage in my work folder)

--- git-archive-all.ps1
+++ git-archive-all.ps1
@@ -10,6 +10,17 @@ if (-not(Test-Path "$REPO_DIR/.git"))
     exit
 }
 
+$status = git status --untracked-files=no --porcelain
+
+if ([string]::IsNullOrEmpty($status)) {
+    # Рабочая директория чиста, за исключением неотслеживаемых файлов
+    Write-Host "OK! Working directory clean excluding untracked files"
+} else {
+    # Есть незакоммиченные изменения в отслеживаемых файлах
+    Write-Host "ERROR! Uncommitted changes in tracked files"
+    exit
+}
+
 $REPO_TREE_ISH = 'HEAD'
 if ($REPO_TREE_ISH -eq $null)
 {
@@ -21,36 +32,41 @@ echo "Change directory $REPO_DIR"
 $RUN_SCRIPT_DIR = $(pwd)
 cd $REPO_DIR
 
-echo "Creating root archive"
+echo "Creating temporary directory"
+$TMP_PATH = "$REPO_DIR/.~tmp_git_archive_all"
+if (Test-Path $TMP_PATH)
+{
+    rmdir $TMP_PATH -r -fo
+}
+mkdir $TMP_PATH -Force > $null
+Set-ItemProperty -LiteralPath $TMP_PATH -Name Attributes -Value Hidden
 
 $REPO_NAME = $(Split-Path $REPO_DIR -Leaf)
 $REPO_HASH = $(git rev-parse --short $REPO_TREE_ISH)
-$ROOT_OUT_PATH = "$REPO_DIR\$REPO_NAME.zip"
+$ROOT_OUT_PATH = "$TMP_PATH\$REPO_NAME.zip"
 
+echo "Creating superproject archive"
 git archive --format zip --output $ROOT_OUT_PATH $REPO_TREE_ISH
 Get-Job | Wait-Job
 
-echo "Creating submodules archive"
-$ARCHIVE_CMD_SUB = 'git archive --prefix $path\ --format zip --output ' + $REPO_DIR + '\$(basename $PWD)-sub-$(git rev-parse --short HEAD).zip ' +  $REPO_TREE_ISH
+echo "Creating submodules archives"
+$ARCHIVE_CMD_SUB = 'git archive --prefix $displaypath\ --format zip --output ' + $TMP_PATH + '\$(basename $PWD)-$(basename $(dirname "$PWD"))-sub-$(git rev-parse --short HEAD).zip ' +  $REPO_TREE_ISH
 $ARCHIVE_CMD_SUB = $ARCHIVE_CMD_SUB -replace '\\', '/'
+echo $ARCHIVE_CMD_SUB
 git submodule foreach --recursive $ARCHIVE_CMD_SUB
 
-echo "Merging archives to one dir"
-Expand-Archive -LiteralPath $ROOT_OUT_PATH -Force -DestinationPath repo
-Get-ChildItem $REPO_DIR -Filter "*-sub*.zip" | Expand-Archive -DestinationPath repo -Force
+echo "Merging archives to one directory"
+Expand-Archive -LiteralPath $ROOT_OUT_PATH -Force -DestinationPath $TMP_PATH/repo
+Get-ChildItem $TMP_PATH -Filter "*-sub*.zip" | Expand-Archive -DestinationPath $TMP_PATH/repo -Force
 
-$(ls *.zip) > repo/hashes.txt
+$(ls $TMP_PATH/*.zip) > $TMP_PATH/repo/hashes.txt
 
-echo "Compress main dir"
+echo "Compress result directory"
 $OUTPUT = "$RUN_SCRIPT_DIR\$REPO_NAME-$REPO_HASH.zip"
-Compress-Archive -Path repo/* -DestinationPath $OUTPUT -Force -CompressionLevel Fastest
+Compress-Archive -Path $TMP_PATH/repo/* -DestinationPath $OUTPUT -Force -CompressionLevel Fastest
 
-echo "Clean files"
-rm -r repo
-rm $ROOT_OUT_PATH
-rm "$REPO_DIR\*sub-*.zip"
+echo "Clean temp"
+rmdir $TMP_PATH -r -fo
 
 echo "Change directory $RUN_SCRIPT_DIR"
 cd $RUN_SCRIPT_DIR
-
-
